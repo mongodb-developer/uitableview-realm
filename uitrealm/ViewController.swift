9930cea6030d8b94582b4ffae7d64834e2e8203b
Michael Lynn <merlynn@gmail.com>
2020-09-15 13:03:15 -0400

    Initial Commit - no realm yet

---
+++ uitrealm/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  uitrealm
+//
+//  Created by Michael Lynn on 9/15/20.
+//  Copyright © 2020 Michael Lynn. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
